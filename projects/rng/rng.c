e23dd9d79dcd84d5121ff9082d85957d045c053b
Tyler Wians <twians18@csl04-l.cornellcollege.edu>
2018-01-19 11:16:58 -0600

    Update RNG

--- projects/rng/rng.c
+++ projects/rng/rng.c
@@ -3,6 +3,7 @@
 #include "lcg.c"
 #include "getfpn.c"
 
+// lcg.c and getfpn.c parameters
 int previousValue = 291;
 int multiplier = 67;
 int increment = 103;
@@ -12,9 +13,17 @@ int main (int argc, char** argv)
 {
   previousValue = linearCongruentialGenerator(previousValue, multiplier, increment, modulus);
 
-  int limit = 20;
-  double set[limit];
+  int limit = 20; // Number of data points
+  double set[limit]; // Array for data points
+  int histogram[10]; // Histogram for data points
+
+  // Clear the space in the array
+  for (int h = 0; h < 10; h++)
+  {
+    histogram[h] = 0;
+  }
   
+  // Get and print values
   for (int i = 0; i < limit; i++)
   {
     double y = getfpn(previousValue, multiplier, increment, modulus);
@@ -22,5 +31,31 @@ int main (int argc, char** argv)
        set[i] = y;
        previousValue = linearCongruentialGenerator(previousValue, multiplier, increment, modulus);
   }
-  
+
+  printf("\n");
+
+  // Print and check values in array
+  for (int j = 0; j < limit; j++)
+  {
+    printf("%.2f\n", set[j]);
+  }
+
+  printf("\n");
+
+  // Get and print group number
+  for (int k = 0; k < limit; k++)
+  {
+    int group = set[k] * 10; // Group number for each value in set
+    printf("%u\n", group);
+    histogram[group]++;
+    
+  }
+
+  printf("\n");
+
+  // Show the results in the histogram
+  for (int l = 0; l < 10; l++)
+  {
+    printf("In group %u, there are %u entries.\n", l, histogram[l]);
+  }
 }
